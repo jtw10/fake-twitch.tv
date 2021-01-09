db3a0db0404f1de18d4b203f600531b24179b8a8
Josh W <jtwong10@gmail.com>
2021-01-09 14:42:42 -0800

    CRUD

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import GoogleAuth from "./streams/GoogleAuth";
+import GoogleAuth from "./GoogleAuth";
 
 const Header = () => {
   return (
